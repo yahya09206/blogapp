163bfb38652a7d570e764cc77d870a6958ada8ac
Yahya Hussein <yahyahussein91@gmail.com>
2017-02-26 18:57:49 -0800

    added code for index route

--- app.js
+++ app.js
@@ -20,9 +20,21 @@ var blogSchema = new mongoose.Schema({
 var Blog = mongoose.model("Blog", blogSchema);
 
 //RESTFUL ROUTES
+app.get("/", function(req,res){
+	res.redirect("/blogs");
+});
+
+
 app.get("/blogs", function(req,res) {
+	Blog.find({}, function(err, blogs) {
+		if(err){
+			console.log(err);
+		}else{
+			res.render("index", {blogs: blogs});
+		}
+	});
 	res.render("index");
-})
+});
 
 
 app.listen(3000, function(){
