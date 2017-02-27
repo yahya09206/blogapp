9f6b0256b0e27a6c496b89f9bb73e97b877e52b2
Yahya Hussein <yahyahussein91@gmail.com>
2017-02-26 18:47:17 -0800

    added comments for more clarity

--- app.js
+++ app.js
@@ -3,12 +3,13 @@ mongoose 		= require("mongoose");
 var express = require("express");
 var app 		= express();
 
+//App Config
 mongoose.connect("mongodb://localhost/blogapp");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-//Schema
+//Mongoose/Model Config
 var blogSchema = new mongoose.schema({
 	title: String,
 	image: String,
@@ -18,6 +19,7 @@ var blogSchema = new mongoose.schema({
 
 var Blog = mongoose.model("Blog", blogSchema);
 
+//RESTFUL ROUTES
 
 app.listen(3000, function(){
 	console.log("You are now listening to the smooth sounds of port3000");
