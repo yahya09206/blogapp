a5ad323b4c6e1d859afbfe33f2978f22ea9cc185
Yahya Hussein <yahyahussein91@gmail.com>
2017-02-26 18:41:26 -0800

    added port to listen for

--- app.js
+++ app.js
@@ -7,3 +7,7 @@ mongoose.connect("mongodb://localhost/blogapp");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlendcoded({extended: true}));
+
+app.listen(3000, function(){
+	console.log("You are now listening to the smooth sounds of port3000");
+});
