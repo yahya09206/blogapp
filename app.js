1bee4fa83e37f7417db55db9d99c08c2be84ed73
Yahya Hussein <yahyahussein91@gmail.com>
2017-02-26 18:42:10 -0800

    fixed error

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 bodyParser 	= require("body-parser");
 mongoose 		= require("mongoose");
-var express = require("exress");
+var express = require("express");
 var app 		= express();
 
 mongoose.connect("mongodb://localhost/blogapp");
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlendcoded({extended: true}));
+
+//Schema
+
 
 app.listen(3000, function(){
 	console.log("You are now listening to the smooth sounds of port3000");
