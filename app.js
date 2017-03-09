4ca545e1043e783e2d68ca594440897e457f89f9
Yahya Hussein <yahyahussein91@gmail.com>
2017-03-09 00:44:30 -0800

    sending to destroy page

--- app.js
+++ app.js
@@ -87,6 +87,9 @@ app.put("/blogs/:id", function(req,res){
 });
 
 //DESTROY ROUTE
+app.delete("/blogs/:id", function(req,res){
+	res.send("You have reached the destroy route");
+});
 
 
 app.listen(3000, function(){
