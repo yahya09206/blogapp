fe73db9bd79ac00ea8fe2091d48fda1a6593232f
Yahya Hussein <yahyahussein91@gmail.com>
2017-03-09 00:56:54 -0800

    sanitized body of blogs

--- app.js
+++ app.js
@@ -46,6 +46,7 @@ app.get("/blogs/new", function(req,res){
 
 //Create Route
 app.post("/blogs", function(req,res){
+	req.body.blog.body = req.sanitize(req.body.blog.body);
 	Blog.create(req.body.blog, function(err,newBlog){
 		if(err){
 			res.render("new");
